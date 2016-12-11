74541a4363e86501f0685e751a7c48cb0ecefef0
Krzysztof Madej <krzysztof.madej.1987@gmail.com>
2016-12-11 11:10:55 +0100

    Changed displaying in SqlProfilerTraceAnalyzer

--- SqlProfilerTraceAnalyzer/SqlProfilerTraceAnalyzer/Program.fs
+++ SqlProfilerTraceAnalyzer/SqlProfilerTraceAnalyzer/Program.fs
@@ -7,28 +7,11 @@ type Trace = XmlProvider<"trace.xml">
 [<EntryPoint>]
 let main argv = 
    
-    let removeSquareBraces (name:string) = 
-        name.Replace("[", String.Empty).Replace("]", String.Empty)
- 
-    let extractSprocName execution =
-        let regex = new Regex(".*exec (?<sproc>\S*)");
-        let result = regex.Match execution
-        let group = result.Groups.["sproc"]
-        if group.Success then Some(removeSquareBraces group.Value) else None
- 
-    // TODO Key not found in case where id column is incorrect
- 
     let extractOptionValue columns id = 
         let result = Array.tryFind (fun (y:Trace.Column) -> y.Id = id) columns
         Option.map (fun (x:Trace.Column) -> x.String) result
  
     let trace = Trace.Load "trace.xml"
-//    let result = trace.Events
-//                    |> Array.map (fun x -> x.Columns)
-//                    |> Array.map (fun x -> 
-//                                            let nameColumn = Array.find (fun (y:Trace.Column) -> y.Id = 1) x
-//                                            let durationColumn = Array.find (fun (y:Trace.Column) -> y.Id = 13) x
-//                                            (nameColumn.String, durationColumn.String))
  
     let getDuration (durationOption: Option<string> option) =
         match durationOption with
@@ -38,6 +21,15 @@ let main argv =
                     | None -> 0
         | None -> 0
  
+    let removeSquareBraces (name:string) = 
+        name.Replace("[", String.Empty).Replace("]", String.Empty)
+ 
+    let extractSprocName execution =
+        let regex = new Regex(".*exec (?<sproc>\S*)");
+        let result = regex.Match execution
+        let group = result.Groups.["sproc"]
+        if group.Success then Some(removeSquareBraces group.Value) else None
+
     let getName (nameOption: Option<string> option) =
         match nameOption with
         | Some x ->
@@ -46,7 +38,7 @@ let main argv =
                     | None -> None
         | None -> None
  
-    let result2 = trace.Events
+    let result = trace.Events
                     |> Array.map (fun x -> x.Columns)
                     |> Array.map (fun x -> (extractOptionValue x 1, extractOptionValue x 13))
                     |> Array.map (fun (nameColumn, durationColumn) -> (getName nameColumn, getDuration durationColumn))
@@ -60,15 +52,9 @@ let main argv =
                                                         let avg = Seq.averageBy (fun (_,duration) -> float duration) entries
                                                         (name, count, sum, avg))
                     |> Seq.sortBy (fun (name,_,_,_) -> name)
-                    |> Seq.map(fun (name,count,sum,avg) -> sprintf "\n%-60s - Count: %-5d - Sum: %-6d - Avg: %-5f" name count sum avg)
+                    |> Seq.map(fun (name,count,sum,avg) -> sprintf "\n%-60s - Count: %-5d - Sum: %-7d - Avg: %-5f" name count sum avg)
                     |> Seq.reduce (sprintf "%s%s")
  
  
-//                    |> Array.concat//                    |> Seq.where (fun x -> x.Id = 1)//                    |> Seq.map (fun x -> x.String)//                    |> Seq.where(fun x -> x.IsSome)//                    |> Seq.map(fun x -> Option.get x)//                    |> Seq.map(fun x -> extractSprocName x)//                    |> Seq.where(fun x -> x.IsSome)//                    |> Seq.map(fun x -> Option.get x)//                    |> Seq.countBy (fun x -> x)//                    |> Seq.sortBy (fun x -> snd x)//                    |> Seq.map(fun (sprocName,number) -> sprintf "%-60s - %d\n" sprocName number)//                    |> Seq.reduce (sprintf "%s%s")
-   
- 
-    printfn "%A" result2
+    printfn "%A" result
     0 // return an integer exit code
- 
- 
-    //http://fsharpforfunandprofit.com/posts/the-option-type/
