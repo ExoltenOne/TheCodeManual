832dded99203ebcc56cf5054d61955bbc051d495
Krzysztof Madej <krzysztof.madej.1987@gmail.com>
2018-11-09 02:17:30 +0100

    Added StringReplacer

---
+++ StringReplacer/StringReplacer.cs
@@ -0,0 +1,80 @@
+﻿namespace StringReplacer
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Order;
+    using System.Linq;
+
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    [MemoryDiagnoser]
+    public class StringReplacer
+    {
+        private readonly Regex pattern = new Regex("[;,\t\r ]|[\n]{2}", RegexOptions.Compiled);
+
+        private readonly char[] separators = { ' ', ';', ',', '\r', '\t', '\n' };
+
+        private readonly string[] separatorsStrings = { " ", ";", ",", "\r", "\t", "\n" };
+
+        [Params(@"this;is,\ra\t\n\n\ntest")]
+        public string Value { get; set; }
+
+        [Benchmark(Baseline = true, Description = "zgirod - MultipleReplaceMethod")]
+        public string MultipleReplaceMethod()
+        {
+            return this.Value.Replace(';', '\n').Replace(',', '\n').Replace('\r', '\n').Replace('\t', '\n')
+                .Replace(' ', '\n').Replace("\n\n", "\n");
+        }
+
+        [Benchmark(Description = "johnluetke - Regex")]
+        public string Regex()
+        {
+            return this.pattern.Replace(this.Value, "\n");
+        }
+
+        [Benchmark(Description = "Paul Walls - ReplaceUsingSplit")]
+        public string ReplaceUsingSplit()
+        {
+            var temp = this.Value.Split(this.separators, StringSplitOptions.RemoveEmptyEntries);
+            return string.Join("\n", temp);
+        }
+
+        [Benchmark(Description = "dodgy_coder - ReplaceUsingAggregate")]
+        public string ReplaceUsingAggregate()
+        {
+            return this.separators.Aggregate(this.Value, (c1, c2) => c1.Replace(c2, '\n'));
+        }
+
+        [Benchmark(Description = "John Whiter - ReplaceUsingStringBuilderAndCharArray")]
+        public string ReplaceUsingStringBuilderAndCharArray()
+        {
+            return StringUtils.ReplaceAny(this.Value, '\n', this.separators);
+        }
+
+        [Benchmark(Description = "Fab - ReplaceUsingStringBuilderAndHashSet")]
+        public string ReplaceUsingStringBuilderAndHashSet()
+        {
+            return StringUtils.MultiReplace(this.Value, this.separators, '\n');
+        }
+
+        [Benchmark(Description = "Daniel Székely - ReplaceInForeach")]
+        public string ReplaceInForeach()
+        {
+            var str = this.Value;
+            foreach (var singleChar in this.separators)
+            {
+                str = str.Replace(singleChar, '_');
+            }
+
+            return str;
+        }
+
+        [Benchmark(Description = "sɐunıɔןɐqɐp - ReplaceUsingStringBuilderAndIndexOf")]
+        public string ReplaceUsingStringBuilderAndIndexOf()
+        {
+            return this.Value.ReplaceAll(this.separatorsStrings, "\n");
+        }
+    }
+}
---
+++ StringReplacer/StringUtils.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+
+namespace StringReplacer
+{
+    using System.Collections.Generic;
+
+    public static class StringUtils
+    {
+        [ThreadStatic]
+        private static StringBuilder m_ReplaceSB;
+
+        private static StringBuilder GetReplaceSB(int capacity)
+        {
+            var result = m_ReplaceSB;
+
+            if (null == result)
+            {
+                result = new StringBuilder(capacity);
+                m_ReplaceSB = result;
+            }
+            else
+            {
+                result.Clear();
+                result.EnsureCapacity(capacity);
+            }
+
+            return result;
+        }
+
+        public static string ReplaceAny(this string s, char replaceWith, params char[] chars)
+        {
+            if (null == chars)
+                return s;
+
+            if (null == s)
+                return null;
+
+            StringBuilder sb = null;
+
+            for (int i = 0, count = s.Length; i < count; i++)
+            {
+                var temp = s[i];
+                var replace = false;
+
+                for (int j = 0, cc = chars.Length; j < cc; j++)
+                    if (temp == chars[j])
+                    {
+                        if (null == sb)
+                        {
+                            sb = GetReplaceSB(count);
+                            if (i > 0)
+                                sb.Append(s, 0, i);
+                        }
+
+                        replace = true;
+                        break;
+                    }
+
+                if (replace)
+                    sb.Append(replaceWith);
+                else
+                if (null != sb)
+                    sb.Append(temp);
+            }
+
+            return null == sb ? s : sb.ToString();
+        }
+
+        public static string MultiReplace(this string s, char[] toReplace, char replacement)
+        {
+            var builder = new StringBuilder(s);
+
+            HashSet<char> set = new HashSet<char>(toReplace);
+            for (int i = 0; i < builder.Length; ++i)
+            {
+                var currentCharacter = builder[i];
+                if (set.Contains(currentCharacter))
+                {
+                    builder[i] = replacement;
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public static string ReplaceAll(this string original, string toBeReplaced, string newValue)
+        {
+            if (string.IsNullOrEmpty(original) || string.IsNullOrEmpty(toBeReplaced)) return original;
+            if (newValue == null) newValue = string.Empty;
+            StringBuilder sb = new StringBuilder();
+            foreach (char ch in original)
+            {
+                if (toBeReplaced.IndexOf(ch) < 0) sb.Append(ch);
+                else sb.Append(newValue);
+            }
+            return sb.ToString();
+        }
+
+        public static string ReplaceAll(this string original, string[] toBeReplaced, string newValue)
+        {
+            if (string.IsNullOrEmpty(original) || toBeReplaced == null || toBeReplaced.Length <= 0) return original;
+            if (newValue == null) newValue = string.Empty;
+            foreach (string str in toBeReplaced)
+                if (!string.IsNullOrEmpty(str))
+                    original = original.Replace(str, newValue);
+            return original;
+        }
+    }
+}
